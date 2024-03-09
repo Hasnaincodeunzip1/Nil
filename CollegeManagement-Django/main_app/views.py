ba1c4b4a5c3959947901b7c4e7ee720488847d64
Hasnain Ahmad Khan <115926521+Hasnaincodeunzip1@users.noreply.github.com>
2024-03-09 17:07:07 +0530

    Update views.py

--- CollegeManagement-Django/main_app/views.py
+++ CollegeManagement-Django/main_app/views.py
@@ -27,29 +27,34 @@ def doLogin(request, **kwargs):
     if request.method != 'POST':
         return HttpResponse("<h4>Denied</h4>")
     else:
-        #Google recaptcha
+        # Google reCAPTCHA
         captcha_token = request.POST.get('g-recaptcha-response')
         captcha_url = "https://www.google.com/recaptcha/api/siteverify"
-        captcha_key = "6LfswtgZAAAAABX9gbLqe-d97qE2g1JP8oUYritJ"
+        captcha_key = settings.RECAPTCHA_SECRET_KEY  # Use the secret key from your settings
+
         data = {
             'secret': captcha_key,
             'response': captcha_token
         }
+
         # Make request
         try:
             captcha_server = requests.post(url=captcha_url, data=data)
             response = json.loads(captcha_server.text)
-            if response['success'] == False:
+
+            if not response['success']:
                 messages.error(request, 'Invalid Captcha. Try Again')
-                return redirect('/')
-        except:
+                return render(request, 'your_login_template.html')
+        except Exception as e:
             messages.error(request, 'Captcha could not be verified. Try Again')
-            return redirect('/')
-        
-        #Authenticate
+            return render(request, 'your_login_template.html')
+
+        # Authenticate
         user = EmailBackend.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
-        if user != None:
+
+        if user is not None and user.is_active:
             login(request, user)
+
             if user.user_type == '1':
                 return redirect(reverse("admin_home"))
             elif user.user_type == '2':
@@ -58,9 +63,9 @@ def doLogin(request, **kwargs):
                 return redirect(reverse("student_home"))
         else:
             messages.error(request, "Invalid details")
-            return redirect("/")
-
-
+            return render(request, 'your_login_template.html')
+        
+       
 
 def logout_user(request):
     if request.user != None:
